18c7d949f4e752ebb89fa4fce33960704c17a8a1
Lars-Erik Aabech <lars-erik@aabech.no>
2026-09-20 10:56:41 +0200

    Make learner equation names case-insensitive

--- src/features/puzzle/compose-puzzle.ts
+++ src/features/puzzle/compose-puzzle.ts
@@ -1,3 +1,4 @@
+import { createCaseInsensitiveLearnerNameResolver } from '../named-expression';
 import type { Relation } from '../problem-model/expression';
 import { collectReferences } from '../problem-model/expression';
 import type { Problem } from '../problem-model/problem';
@@ -145,7 +146,7 @@ export function submitPuzzle(
     problem: options.problem,
     locale: options.locale,
     answer: options.answer,
-    names: presentation.learnerNames,
+    names: createCaseInsensitiveLearnerNameResolver(presentation.learnerNames),
   });
   return mergeModeWithTheme(options, { state, feedback, submission });
 }
